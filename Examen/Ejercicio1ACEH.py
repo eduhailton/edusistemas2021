9424c3289786960f4c2ed010bcb4cfbf9a002e27
eduhailton <>
2021-10-10 17:33:12 +0000

    Examen

---
+++ Examen/Ejercicio1ACEH.py
@@ -0,0 +1,9 @@
+print("******Calculat Promedio final******")
+#Datos de entrada
+nota1=float(input("Ingrese nota 1:"))
+nota2=float(input("Ingrese nota 2:"))
+nota3=float(input("Ingrese nota 3:"))
+#Proceso
+promedioFin=(nota1*0.25)+(nota2*0.25)+(nota3*0.50)
+#Datos de Salida
+print(f"Su promedio final es: {round(promedioFin,2)}")
